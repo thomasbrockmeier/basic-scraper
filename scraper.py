a62f724121f34a9a8ec2c41f08fe3abb6dc9ba30
Thomas <t.brockmeier@youngcapital.nl>
2017-03-25 17:38:41 +0100

    scrape distill.pub chaneels

--- scraper.py
+++ scraper.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+from datetime import datetime
 from lxml import html
 from functools import reduce
 import pandas as pd
@@ -10,14 +11,25 @@ path = "/rss.xml"
 def ingredients(*args):
     return reduce(lambda x, y: f"{x}{y}", args)
 
-def soupify(url):
+def soupify(url, xml=True):
     try:
         r = requests.get(url)
     except HTTPError as e:
         print(e)
         return None
     try:
-        return BeautifulSoup(r.content, 'lxml')
+        return BeautifulSoup(r.content, f"lxml{'-xml' if xml else ''}")
     except Exception as e:
         print(e)
         return None
+
+def channel_to_df(channel):
+    titles, urls, publish_dates = [list(map(lambda x: getattr(x, col).text, channel.find_all('item'))) for col in ['title', 'guid', 'pubDate']]
+    df = pd.DataFrame({'title': titles, 'url': urls, 'publish_date': publish_dates})
+    df['publish_date'] = pd.to_datetime(df['publish_date'], utc = True)
+    return df
+
+def process(soup):
+    return [channel_to_df(channel) for channel in soup.find_all('channel')]
+
+channels = process(soupify(ingredients(url, path)))
